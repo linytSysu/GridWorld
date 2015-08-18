2a68df469b892aa767a4d4f5d9243e1d5f1bf60a
linytSysu <linytsysu@163.com>
2015-08-18 18:57:49 +0800

    day1

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+	private String str;
+	public void hello() {
+		str = "Hello World!";
+	}
+	public String getStr() {
+		return str;
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ HelloWorld/test/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HelloWorldTest {
+	public HelloWorld h = new HelloWorld();
+	@Test
+	public void testHello() {
+		h.hello();
+		assertEquals("Hello World", h.getStr());
+	}
+}
+
